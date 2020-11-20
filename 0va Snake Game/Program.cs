2f73187da2ac1cad629dd39484c262f402fe6f2c
Mikhail Bounoung <67975897+Merca0va@users.noreply.github.com>
2020-11-20 12:49:20 +0000

    Update Program.cs

--- 0va Snake Game/Program.cs
+++ 0va Snake Game/Program.cs
@@ -16,7 +16,7 @@ namespace _0va_Snake_Game
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form0());
         }
     }
 }
